a29290f6b8e5e8633c09b1ddfe261b64fc8bfe26
0xVorian <33686588+0xVorian@users.noreply.github.com>
2025-08-01 15:59:27 +0200

    up

--- src/utils.ts
+++ src/utils.ts
@@ -5,6 +5,11 @@ const tokens = [
     decimals: 18,
   },
   {
+    address: "0x0000000000000000000000000000000000000000",
+    name: "ETH",
+    decimals: 18,
+  },
+  {
     address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
     name: "USDC",
     decimals: 6,
