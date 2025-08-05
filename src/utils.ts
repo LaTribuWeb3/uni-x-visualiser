ecad03d981062054e5b0382b36301c26fa673f62
0xVorian <33686588+0xVorian@users.noreply.github.com>
2025-08-05 14:30:24 +0200

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -94,7 +94,7 @@ const tokens = [
 ];
 
 export const getTokenName = (tokenAddress: string) => {
-  return tokens.find((token) => token.address === tokenAddress)?.name || "Unknown";
+  return tokens.find((token) => token.address === tokenAddress)?.name || tokenAddress;
 };
 
 export const getTokenDecimals = (tokenAddress: string) => {
