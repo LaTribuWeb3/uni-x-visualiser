f742fdeee99395907275fee416ab00ebbc7d296f
0x3lden <153280243+0x3lden@users.noreply.github.com>
2025-08-19 17:15:43 +0200

    Adds deployment script

---
+++ deploy_enrichment_and_restart.sh
@@ -0,0 +1,12 @@
+eval $(ssh-agent -s) 
+ssh-add <(echo "$PRIVATE_KEY") 
+  
+# List out your new key's fingerprint
+ssh-add -l
+
+scp package.json root@91.98.78.13:/opt/bulk-enrichment
+scp dist/scripts root@91.98.78.13:/opt/bulk-enrichment
+ssh root@91.98.78.13 "pm2 restart bulkEnrichment"
+
+# Don't forget to cleanup your agent after you're done using it if you're not on an ephemeral build server.
+ssh-agent -k
