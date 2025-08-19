83d69c6d4cfaa5edb21aeeb18f47eb045e345c68
0x3lden <153280243+0x3lden@users.noreply.github.com>
2025-08-19 18:32:18 +0200

    recursive scp

--- deploy_enrichment_and_restart.sh
+++ deploy_enrichment_and_restart.sh
@@ -5,7 +5,7 @@ ssh-add <(echo "$PRIVATE_KEY")
 ssh-add -l
 
 scp package.json root@91.98.78.13:/opt/bulk-enrichment
-scp dist/scripts root@91.98.78.13:/opt/bulk-enrichment
+scp -r dist/scripts root@91.98.78.13:/opt/bulk-enrichment
 ssh root@91.98.78.13 "pm2 restart bulkEnrichment"
 
 # Don't forget to cleanup your agent after you're done using it if you're not on an ephemeral build server.
