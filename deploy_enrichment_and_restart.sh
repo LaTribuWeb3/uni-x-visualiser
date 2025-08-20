d423ae64bb92d608ccaebb653c400a0561d32dc6
0x3lden <153280243+0x3lden@users.noreply.github.com>
2025-08-20 10:15:23 +0200

    Debug directly in debug script

--- deploy_enrichment_and_restart.sh
+++ deploy_enrichment_and_restart.sh
@@ -1,12 +1,23 @@
+echo "Evaluating ssh agent"
+
 eval $(ssh-agent -s) 
-ssh-add <(echo "$PRIVATE_KEY") 
+
+echo "Adding private key"
+
+ssh-add <(echo "$PRIVATE_KEY")
+
+echo "Verifying private keys"
   
 # List out your new key's fingerprint
 ssh-add -l
 
-scp package.json root@91.98.78.13:/opt/bulk-enrichment
-scp -r dist/scripts root@91.98.78.13:/opt/bulk-enrichment
-ssh root@91.98.78.13 "pm2 restart bulkEnrichment"
+echo "Copying files"
+
+scp -vvv package.json root@91.98.78.13:/opt/bulk-enrichment
+scp -vvv -r dist/scripts root@91.98.78.13:/opt/bulk-enrichment
+ssh -vvv root@91.98.78.13 "pm2 restart bulkEnrichment"
+
+echo "Cleanup ssh agent"
 
 # Don't forget to cleanup your agent after you're done using it if you're not on an ephemeral build server.
 ssh-agent -k
